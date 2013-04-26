47e2917ba1005c33adc9e17cd0fff483b7f2d07d
Tim Boudreau <niftiness@gmail.com>
2013-04-26 15:17:35 -0400

    X

--- acteur-auth/src/main/java/com/mastfrog/acteur/auth/CookieAuthenticationStrategy.java
+++ acteur-auth/src/main/java/com/mastfrog/acteur/auth/CookieAuthenticationStrategy.java
@@ -1,9 +1,21 @@
 package com.mastfrog.acteur.auth;
 
+import com.mastfrog.acteur.Event;
+
 /**
  *
  * @author tim
  */
-public class CookieAuthenticationStrategy {
+public class CookieAuthenticationStrategy extends AuthenticationStrategy {
+
+    @Override
+    protected Result<?> authenticate(Event evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected void onAuthenticationFailed(Event evt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
